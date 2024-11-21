ac94a07943e23d9d373f074d6b4577bc48cd32da
RuuRoger <lozanofierroruu@gmail.com>
2024-11-21 23:10:59 +0100

    I put new colliders in the trees, polygon colliders. This is because i have a new idea with mecanichs and level design. Now, the ball is smaller and you can evite the trees and velocity effect is better. I tried to put a platform speed. This triplicate your velocity but i coudn't fit in with all yet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Public properties
     public float speed;
 
     private void Update()
     {
+        //Player movement
         transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
+
+        print("Velocidad :" + speed);
+
     }
+
+    public void MoreSpeed() => speed *= 3; 
+
 }
