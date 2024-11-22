549bc5efd16b3cad3ec882c9aae2a6f8b62bb5d2
RuuRoger <lozanofierroruu@gmail.com>
2024-11-22 08:57:35 +0100

    Finally, i remove the speed platform because i saw that doesn't alterate the velocity because the velocity's ball is gravity. I add new code: now the ball turns red if you hit with something and if you have another hit, it finish the game

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,15 +7,29 @@ public class Player : MonoBehaviour
     //Public properties
     public float speed;
 
+    //Private attributes
+    private byte _hits;
+
+    //Methods
+    private void Start()
+    {
+        _hits = 0;
+    }
+
     private void Update()
     {
         //Player movement
         transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed);
 
-        print("Velocidad :" + speed);
-
+        //Game over
+        if (_hits == 3) Time.timeScale = 0;
     }
 
-    public void MoreSpeed() => speed *= 3; 
+    private void OnCollisionEnter(Collision collision)
+    {
+        _hits++;
 
+        if (_hits == 2) gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
 }
+
